f1a5067bc3e6df083078e7dd8b7d61066c9b359c
Manju <manjunath.damodharan@dazn.com>
2020-08-12 18:58:14 +0200

    jsonplaceHolder Test

---
+++ src/test/java/rMethods/RestCalls.java
@@ -0,0 +1,28 @@
+package rMethods;
+
+import io.restassured.response.Response;
+import testData.API_Constants;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.with;
+
+public class RestCalls extends API_Constants {
+
+    public static Response getRequest(String URI) {
+        return given().get(JSON_PLACEHOLDER_HOST + URI);
+    }
+
+    public static Response getRequestWithParameter(String URI, String parameterKey, String parameterValue) {
+        return with().params(parameterKey, parameterValue).given().get(JSON_PLACEHOLDER_HOST + URI);
+    }
+
+    public static Response getRequestWithParameters(String URI, Map<String, String> parameters) {
+        return with().params(parameters).given().get(JSON_PLACEHOLDER_HOST + URI);
+    }
+
+    public static Response postRequest(String body, String URI) {
+        return with().body(body).request("POST", JSON_PLACEHOLDER_HOST + URI);
+    }
+}
---
+++ src/test/java/testData/API_Constants.java
@@ -0,0 +1,8 @@
+package testData;
+
+public class API_Constants {
+    public static String JSON_PLACEHOLDER_HOST = "https://jsonplaceholder.typicode.com";
+    public static String USERS = "/users";
+    public static String POSTS = "/posts";
+    public static String COMMENTS = "/comments";
+}
